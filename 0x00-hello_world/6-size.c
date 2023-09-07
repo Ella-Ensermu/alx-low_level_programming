5eb0d423058e1f41c77a5b7f25e120aa18227f4a
Ella_Ensermu <elroeensermu970@gmail.com>
2023-09-07 14:43:48 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various computer types.
+ * main - prints the size of various types.
  *
  * Return: Always 0 (Success)
  */
@@ -14,5 +14,5 @@ int main(void)
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
 
-	return(0);
+	return (0);
 }
