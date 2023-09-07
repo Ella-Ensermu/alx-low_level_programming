85d3db50acfc3ce7a22f4a66e4877b1ad99e2ffe
Ella_Ensermu <elroeensermu970@gmail.com>
2023-09-07 13:08:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
- * main - Entry point
- * 
- * Return: Always 0 (Success)
+ * main - A program that prints a line with puts functio
+ n
+ * Return: 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle");
+	puts("\"Programing is like building a multilingual puzz
+le");
 	return (0);
 }
