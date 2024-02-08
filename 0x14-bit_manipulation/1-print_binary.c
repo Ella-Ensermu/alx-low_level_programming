94390ba621b3d9122680112cf837d4fd3a528eba
Ell-Ensermu <elroeensermu970@gmail.com>
2024-02-09 00:03:48 +0300

    0x14-bit_manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_binary - a function that converts a binary number to decimal
+ * @n: a decimal value
+ * Return: null
+ */
+void print_binary(unsigned long int n)
+{
+	int i, zero = 1;
+	unsigned long int m;
+	int f;
+
+	f = sizeof(unsigned long int) * 8 - 1;
+
+	for (i = f; i >= 0; i--)
+	{
+		m = 1UL << i;
+		if (n & m)
+		{
+			_putchar('1');
+			zero = 0;
+		}
+		else if (!zero)
+		{
+			_putchar('0');
+	}
+		m >>= 1;
+	}
+
+	if (zero)
+	{
+		_putchar('0');
+	}
+
+}
