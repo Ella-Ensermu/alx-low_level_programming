964b4f1eb920c5fab423efce75ab1c924597cc73
Ella-Ensermu <142889164+Ella-Ensermu@users.noreply.github.com>
2024-03-21 21:54:05 +0300

    Add files via upload

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,18 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H
+#define _LISTS_H
 
-#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
 typedef struct dlistint_s
 {
 	int n;
